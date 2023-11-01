78fb96f2629929336a1dde73b3abe70bd477e2c0
terisuke <terisuke1115@gmail.com>
2023-11-01 14:32:58 +0900

    create app

---
+++ lib/pages/feed_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+class FeedPage extends StatefulWidget {
+  @override
+  _FeedPageState createState() => _FeedPageState();
+}
+
+class _FeedPageState extends State<FeedPage> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'INSTAGRAM',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                  color: Colors.grey),
+            ),
+            Text(
+              '投稿',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+          ],
+        ),
+        actions: [Container(width: 48)],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage('https://img.freepik.com/premium-vector/instagram-app-icon-social-media-logo-vector-illustration_277909-403.jpg?w=360'),
+                  ),
+                  title: Container(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('instagram_clone✅', style: TextStyle(fontWeight: FontWeight.bold),),
+                        Text('日本のどこか', style: TextStyle(fontSize: 12),),
+                      ],
+                    ),
+                  ),
+                  trailing: Icon(Icons.more_horiz),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.width * 1.2,
+                  width: MediaQuery.of(context).size.width,
+                  child: PageView(
+                    controller: _pageController,
+                    onPageChanged: (index) {
+                      setState(() {
+                        _currentPage = index;
+                      });
+                    },
+                    children: [
+                      Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2TS5E_z1bPd3XUvg2uwYTEJM1BMtE145yA7AvnGdEjjiRNZrxBdPAVkhfO2BH8FvTQCk&usqp=CAU', fit: BoxFit.cover,),
+                      Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy359-pcpDNHVQVptP04eGbx2mygdIf3syaaJkc_1vsZNJ2TYutjKvjdBjhPl4bt4WeCY&usqp=CAU', fit: BoxFit.cover,),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(2, (index) {
+                    return GestureDetector(
+                      onTap: () {
+                        _pageController.animateToPage(index, duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: CircleAvatar(
+                          radius: 5,
+                          backgroundColor: _currentPage == index ? Colors.blue : Colors.grey,
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 4.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.favorite_border),
+                          SizedBox(width: 8.0),
+                          Icon(Icons.chat_bubble_outline),
+                          SizedBox(width: 8.0),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Icon(Icons.send),
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.bookmark_border),
+                    ],
+                  ),
+                ),
+                Text(
+                  '「いいね！」704,899件',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 4.0),
+                Text('なぁ〜にぃ〜？やっちまったな！', style: TextStyle()),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/my_page.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatelessWidget {
+  MyPage({Key? key}) : super(key: key);
+  final Images = [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2TS5E_z1bPd3XUvg2uwYTEJM1BMtE145yA7AvnGdEjjiRNZrxBdPAVkhfO2BH8FvTQCk&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy359-pcpDNHVQVptP04eGbx2mygdIf3syaaJkc_1vsZNJ2TYutjKvjdBjhPl4bt4WeCY&usqp=CAU'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('instagram_clone✅',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Image.network(
+                    'https://img.freepik.com/premium-vector/instagram-app-icon-social-media-logo-vector-illustration_277909-403.jpg?w=360',
+                    width: 100,
+                    height: 100,
+                  ),
+                  const Spacer(),
+                  const Column(
+                    children: [
+                      Text('7,041',
+                      style:TextStyle(
+                        fontWeight:FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                      ),
+                      Text('投稿'),
+                      
+                    ],
+                  ),
+                  const SizedBox(width: 16),
+                  const Column(
+                    children: [
+                      Text('1,000',
+                      style:TextStyle(
+                        fontWeight:FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                      ),
+                      Text('フォロワー'),
+                    ],
+                  ),
+                  const SizedBox(width: 16),
+                  const Column(
+                    children: [
+                      Text('1,000',
+                      style:TextStyle(
+                        fontWeight:FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                      ),
+                      Text('フォロー'),
+                    ],
+                  ),
+                ],
+              ),
+              const Text('Instagram',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+              ),
+              const Text('#YoursToMake',
+              style: TextStyle(
+                color: Colors.blue
+              ),
+              ),
+              const Text('help.instagram.com',
+              style: TextStyle(
+                color: Colors.blue
+              ),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: OutlinedButton(
+                            onPressed: (){},
+                            child: const Text('フォロー中',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                              ),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 4),
+                        Expanded(
+                          child: OutlinedButton(
+                            onPressed: (){},
+                            child: const Text('メッセージ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                              ),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 4),
+                  OutlinedButton(
+                    onPressed: (){},
+                    child: const Icon(Icons.arrow_drop_down),
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 3,
+                children: Images.map((imageUrl) => InstagramPostItem(imageUrl: imageUrl)).toList(),
+              ),
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+}
+class InstagramPostItem extends StatelessWidget {
+  const InstagramPostItem({Key? key, required this.imageUrl}) : super(key: key);
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      imageUrl,
+      fit: BoxFit.cover,
+    );
+  }
+}
